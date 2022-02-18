776af7f72ce4696b4a662ee3253800322a069aac
Alejandro Henestroza <alehenestroza@gmail.com>
2022-02-18 12:58:27 -0300

    Created Spring Boot project

---
+++ backend/src/main/java/ar/com/alehenestroza/todoapi/TodoApiApplication.java
@@ -0,0 +1,13 @@
+package ar.com.alehenestroza.todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApiApplication.class, args);
+    }
+
+}
