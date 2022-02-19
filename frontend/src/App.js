756d9945c262c50d9bbe380465cd3f4d1a51d81e
Alejandro Henestroza <alehenestroza@gmail.com>
2022-02-18 21:59:46 -0300

    Added Axios package to React App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import TodoForm from "./components/NewTodo/TodoForm";
 import TodoList from "./components/UI/TodoList";
 
 const App = () => {
+  const axios = require("axios");
+  axios.get(`${process.env.REACT_APP_API_URL}/todos`).then((res) => {
+    console.log(res.data);
+  });
   const todos = [
     { id: 1, text: "Learn React", completed: false },
     { id: 2, text: "Learn Redux", completed: false },
